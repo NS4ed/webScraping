a50f9ce638daf9f4efe423fd27e936a1a775ef2c
derecksdev <73000078+derecksdev@users.noreply.github.com>
2021-04-19 17:55:16 -0400

    Update stateLevel_GS.js

--- LMI Scraping/stateLevel_GS.js
+++ LMI Scraping/stateLevel_GS.js
@@ -16,24 +16,34 @@ https.globalAgent.options.ca = sslRootCAs;
 //const getUrls = require('get-urls');
 //const cors = require('cors')({origin: true});
 
-// Redirect stderr to 'stderr.txt'/'failed.txt'
+// Redirect stderr/stdout to 'stderr.txt'/'stdout.txt'
 let redofile = fs.createWriteStream("failed.txt");
 let errfile = fs.createWriteStream("stderr.txt"); 
 process.stderr.write = function(data) {
     errfile.write(data);
 };
 
-// Traverses the JSON and scrap each link for state-level data
+// Traverse the JSON and scrap each link for state-level data
 async function traverse(jsonObj){
-    var j = 0;
+   /* var j = 0;
     for (i in jsonObj){
         link = jsonObj[i]['link'];
         dir = i;
         
         console.log("Gathering state level data for", dir, "... ");
-        if ((j + 1) % 2 == 0) await scrapBLSStateLvl(link, dir);
-        else scrapBLSStateLvl(link, dir);
+        if ((j + 1) % 2 == 0) await scrapBLS_GS(link, dir);
+        else scrapBLS_GS(link, dir);
+        //scrapBLS_GS(link, dir);
+        //break;
         j++;
+    }*/
+
+    for (i in jsonObj){
+        link = jsonObj[i]['link'];
+        dir = i;
+
+        scrapBLS_GS(link, dir, 1);
+        break;
     }
 
 }
@@ -44,13 +54,14 @@ async function click_xpath(page, xpath){
         var elements = await page.$x(xpath);
         await elements[0].click();
     } catch (error) {
-        await click_xpath(page, xpath);
+        var elements = await page.$x(xpath);
+        await elements[0].click();
     }
     return;
 }
 
 // Scraps a given webpage for state-level data
-async function scrapBLSStateLvl(url, state) {
+async function scrapBLS_GS(url, state, flag = 0) {
     //const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4403.0 Safari/537.36';
     let browser = await pupp.launch({headless: false});
     var elements;
@@ -61,10 +72,18 @@ async function scrapBLSStateLvl(url, state) {
     await page.setRequestInterception(true);
     //page.setDefaultTimeout(15000);
 
-    await page._client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        downloadPath: './Excel_Sheets/'+state
-      });
+    if (flag != 0){
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: './Excel_Sheets/EP/regionLvl/'+state
+        });
+    }
+    else {
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: './Excel_Sheets/EP/stateLvl/'+state
+        });
+    }
 
     page.on('request', (req) => {
       if(req.resourceType() == 'font' || req.resourceType() == 'image'){
@@ -87,10 +106,29 @@ async function scrapBLSStateLvl(url, state) {
     await page.waitFor('#filtersetIcon');
     await page.evaluate(()=>document.querySelector('#filtersetIcon').click()); 
 
+    // For WORKFORCE-REGION DATA
+    if (flag != 0){
+        // Set geography type (Workforce Region/Area)
+        await page.waitForXPath('//*[@id="ana-area-filter-body"]/div/div[2]/div/button');
+        page.waitFor(1500);
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[2]/div/button');
+        
+        page.waitFor(2000);
+        await page.waitForXPath('//*[@id="ana-area-filter-body"]/div/div[2]/div/div/div[2]/div/button[2]');
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[2]/div/div/div[2]/div/button[2]');
+
+        page.waitFor(2000);
+        await page.waitForXPath('//*[@id="ana-area-filter-body"]/div/div[2]/div/div/div[3]/ul/li[3]/a');
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[2]/div/div/div[3]/ul/li[3]/a');
+
+        page.waitFor(1500);
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[2]/div/button');
+    }
 
     // Set 'Occupation Level' filter
     await page.waitFor(2000);
     await page.waitForXPath('//*[@id="ana_occ_level_list"]/div/button', {visible: true}); // Open Dropdown menu for Occ. Level
+    await page.waitFor(2000);
     await click_xpath(page, '//*[@id="ana_occ_level_list"]/div/button');
     
 
@@ -130,8 +168,23 @@ async function scrapBLSStateLvl(url, state) {
     await click_xpath(page, '//*[@id="ana_occ_codes_list"]/div/button'); // Close Dropdown menu
     console.log("\t\t(" + state + ") OCCs Set");
 
+    // For WORKFORCE-REGION DATA
+    if (flag != 0){
+        // Select all regions
+        await page.waitFor(1500);
+        await page.waitForXPath('//*[@id="ana-area-filter-body"]/div/div[3]/div/button');   // Open dropdown menu
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[3]/div/button');   // for region selection
+
+        await page.waitFor(2000);
+        await page.waitForXPath('//*[@id="ana-area-filter-body"]/div/div[3]/div/div/div[2]/div/button[1]'); // Click 
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[3]/div/div/div[2]/div/button[1]'); // 'select all'
+
+        await click_xpath(page, '//*[@id="ana-area-filter-body"]/div/div[3]/div/button'); // Close dropdown menu
+    }
+
     // Download Excel Sheet
-    await page.waitFor(10000); 
+    if (flag != 0) await page.waitFor(7000);
+    await page.waitFor(15000); 
     await page.waitForXPath('//*[@id="maintable_wrapper"]/div[4]/div[2]/div/button[3]', {visible: true}); // Download Excel
     await click_xpath(page, '//*[@id="maintable_wrapper"]/div[4]/div[2]/div/button[3]');  
     }
@@ -140,12 +193,13 @@ async function scrapBLSStateLvl(url, state) {
         redofile.write(state, " - Failed to download state-level data\n");
         return;
     }
-    console.log("\n(" + state + ") The state-level data for " + state + " has been collected. File downloaded to ./Excel_Sheets/" + state + "\n");
+    console.log("\n(" + state + ") The data for " + state + " has been collected. File downloaded to the ./Excel_Sheets folder.\n");
 
     await page.waitFor(5000); 
     
     browser.close();
 }
+
 
 // Traverse the JSON and scrap web pages
 traverse(sLLJson);
